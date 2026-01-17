aacde45a1b9e78698835a73f8311007fe0366bdf
GugiChu <gauravchhajer613nkh@gmail.com>
2026-01-17 22:06:57 +0530

    jan 17

---
+++ day-1/app.js
@@ -0,0 +1,7 @@
+// console.log('node <filename.js>')
+// const catMe = require('cat-me');
+// console.log(catMe())
+
+const express = require('express');
+const app = express(); // server creation 
+app.listen(3000); // starting server
