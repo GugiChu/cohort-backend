4bc6a4a69928175779280ac1941839e1cb98cf2d
GugiChu <gauravchhajer613nkh@gmail.com>
2026-01-22 22:02:25 +0530

    jan 22

---
+++ day-3/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json()); 
+const notes = [];
+
+app.post('/',(req,res)=>{
+    notes.push(req.body)
+    res.send('Note created')
+});
+
+app.get('/',(req,res)=>{
+    res.send(notes)
+})
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+});
